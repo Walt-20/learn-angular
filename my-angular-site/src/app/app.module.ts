11540f4e6a30ed51e16834a36154c6309571840c
Walt-20 <walt.c0des20@gmail.com>
2023-10-29 10:00:54 -0600

    refactoring into core

--- my-angular-site/src/app/app.module.ts
+++ my-angular-site/src/app/app.module.ts
@@ -4,18 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { FormsModule } from '@angular/forms';
-import { UserService } from './user.service';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     HomeComponent,
     ContactComponent
   ],
@@ -23,10 +19,10 @@ import { UserService } from './user.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [
-    UserService
   ],
   bootstrap: [AppComponent]
 })
--- my-angular-site/src/app/core/components/footer/footer.component.ts
+++ my-angular-site/src/app/core/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <footer class="footer">
       <div class="container content has-text-centered">
-        <p>Made with ❤️ by Walt from Scotch</p>
+        <p>Made with ❤️ by Walt from <a href="https://www.youtube.com/watch?v=5IQn1HE1_m8&list=PL52V8-tsPGpPAvlzYoxCIIHaEFkvjXdho">Scotch</a></p>
       </div>
     </footer>
   `,
